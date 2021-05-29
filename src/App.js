f54066d81236b471ad10729aa006239062128610
wahid_abdul <abdulwahid.ag@marketsimplified.com>
2021-05-29 21:22:47 +0530

    - inital commit - a draggable div floating around

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import dragElement from './utils/dragEnabler';
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+    dragElement(document.getElementById("mydivheader"));
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="terminal" id="mydivheader"></div>
     </div>
   );
 }
---
+++ src/utils/dragEnabler.js
@@ -0,0 +1,35 @@
+
+const dragElement = (elm) => {
+    var pos1, pos2, pos3, pos4;
+    elm.onmousedown = dragMouseDown;
+  
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      //get x , y when i click the mouse
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmousemove = dragMouseMove;
+      document.onmouseup = dragMouseUp;
+    }
+  
+    function dragMouseMove(e) {
+      e = e || window.event;
+      e.preventDefault();
+      //get x , y when i move the mouse
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      //update
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      elm.style.top = elm.offsetTop - pos2 + "px";
+      elm.style.left = elm.offsetLeft - pos1 + "px";
+    }
+  
+    function dragMouseUp() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
+  
+  export default dragElement;
