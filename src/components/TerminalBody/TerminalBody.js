945b7a72e70a38f00c4a5d61a4aea2e8799586f3
Wahid-Abdul <awahidag@gmail.com>
2021-07-07 20:14:43 +0530

    - record history - auto scroll to end

--- src/components/TerminalBody/TerminalBody.js
+++ src/components/TerminalBody/TerminalBody.js
@@ -4,7 +4,20 @@ import "./TerminalBody.css"
 const TerminalBody = () => {
 
     const [fullCommand, setFullCommand] = useState("")
+    const [currentPath, setCurrentPath] = useState("/")
+    const [history, setHistory] = useState([])
+
     const inputRef = React.useRef();
+
+    const scrollToBottom = (element) => {
+
+        element.scrollTo({
+            top: element.scrollHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
     useEffect(() => {
         focusInput();
     }, [])
@@ -14,6 +27,20 @@ const TerminalBody = () => {
 
     const focusInput = () => {
         inputRef.current.focus()
+    }
+
+    const executeCommand = () => {
+        const [command, argument, options] = fullCommand.split(" ")
+        console.log(command, argument, options)
+    }
+
+    const recordHistory = () => {
+        const currentDetails = {
+            path: currentPath,
+            command: fullCommand,
+        }
+        setHistory([...history, currentDetails])
+        setFullCommand("")
     }
 
     const onChangeText = (e) => {
@@ -26,18 +53,26 @@ const TerminalBody = () => {
 
             case KEY_CODES.ENTER:
                 e.preventDefault();
+                recordHistory(currentPath, fullCommand)
+                executeCommand();
                 break;
 
             default:
                 break;
         }
+        const terminalBody = document.getElementById("terminalBody")
+        scrollToBottom(terminalBody)
     }
 
+    const renderHistory = history.map((item) => <>{`${item.path}$ ${item.command}`}<br /></>)
+
     return (
-        <div className="terminalBody" onClick={onClickBody}>
+        <div className="terminalBody" onClick={onClickBody} id="terminalBody">
             <div className="textContainer">
-                $&nbsp;{fullCommand}
+                {renderHistory}
+                {currentPath}$&nbsp;{fullCommand}
                 <input className="commandInput"
+                    value={fullCommand}
                     type="hidden"
                     type="text"
                     onChange={(e) => setFullCommand(e.target.value)}
